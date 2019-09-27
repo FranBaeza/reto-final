a630bc75230d74deb43ef48aef7626eb97c5b114
Fran <>
2019-09-27 11:24:35 +0200

    primer commit rama front

---
+++ front/src/app/ofertas/ofertas.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { OfertasRoutingModule } from './ofertas-routing.module';
+import { OfertasListComponent } from './ofertas-list/ofertas-list.component';
+import { OfertasDetailComponent } from './ofertas-detail/ofertas-detail.component';
+import { OfertasEditComponent } from './ofertas-edit/ofertas-edit.component';
+import { OfertasAddComponent } from './ofertas-add/ofertas-add.component';
+import { OfertasService } from './ofertas.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    OfertasRoutingModule
+  ],
+  declarations: [
+    OfertasListComponent,
+    OfertasDetailComponent,
+    OfertasEditComponent,
+    OfertasAddComponent
+  ],
+  providers: [OfertasService]
+
+})
+export class OfertasModule { }
