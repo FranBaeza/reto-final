f0b281760322bb3aa96ee774c55e84e7f60ffe17
Fran <>
2019-09-27 12:06:07 +0200

    bug1 owner edit

--- front/src/app/owners/owner-edit/owner-edit.component.ts
+++ front/src/app/owners/owner-edit/owner-edit.component.ts
@@ -21,42 +21,42 @@
  */
 
 import {Component, OnInit} from '@angular/core';
-import {OfertaService} from '../ofertas-add';
-import {Oferta} from '../ofertas';
+import {OwnerService} from '../owner.service';
+import {Owner} from '../owner';
 import {ActivatedRoute, Router} from '@angular/router';
 import 'rxjs/Rx';
 
 @Component({
-  selector: 'app-oferta-edit',
-  templateUrl: './oferta-edit.component.html',
-  styleUrls: ['./oferta-edit.component.css']
+  selector: 'app-owner-edit',
+  templateUrl: './owner-edit.component.html',
+  styleUrls: ['./owner-edit.component.css']
 })
-export class OfertaEditComponent implements OnInit {
-  oferta: Oferta;
+export class OwnerEditComponent implements OnInit {
+  owner: Owner;
   errorMessage: string; // server error message
 
-  constructor(private ofertaService: OfertaService, private route: ActivatedRoute, private router: Router) {
-    this.oferta = <Oferta>{};
+  constructor(private ownerService: OwnerService, private route: ActivatedRoute, private router: Router) {
+    this.owner = <Owner>{};
   }
 
   ngOnInit() {
-    const ofertaId = this.route.snapshot.params['id'];
-    this.ofertaService.getOfertaById(ofertaId).subscribe(
-      oferta => this.oferta = oferta,
+    const ownerId = this.route.snapshot.params['id'];
+    this.ownerService.getOwnerById(ownerId).subscribe(
+      owner => this.owner = owner,
       error => this.errorMessage = <any> error);
   }
 
-  onSubmit(oferta: Oferta) {
+  onSubmit(owner: Owner) {
     var that = this;
-    this.ofertaService.updateOferta(oferta.id.toString(), oferta).subscribe(
-      res => this.gotoOfertaDetail(oferta),
+    this.ownerService.updateOwner(owner.id.toString(), owner).subscribe(
+      res => this.gotoOwnerDetail(owner),
       error => this.errorMessage = <any> error
     )
   }
 
-  gotoOfertaDetail(oferta: Oferta) {
+  gotoOwnerDetail(owner: Owner) {
     this.errorMessage = null;
-    this.router.navigate(['/ofertas', oferta.id]);
+    this.router.navigate(['/owners', owner.id]);
   }
 
 
