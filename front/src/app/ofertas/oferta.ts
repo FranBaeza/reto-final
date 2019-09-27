6517740c9669eb633ce7d7430ad0cada0ec2717c
mobej1979 <>
2019-09-27 11:26:07 +0200

    interfazYEdit

---
+++ front/src/app/ofertas/oferta.ts
@@ -0,0 +1,8 @@
+
+export interface Oferta {
+    id: number;
+    title: string;
+    descripcion: string;
+    discount: number;
+    expiredate: Date;
+}
--- front/src/app/ofertas/ofertas-detail/ofertas-detail.component.ts
+++ front/src/app/ofertas/ofertas-detail/ofertas-detail.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OfertasService} from '../ofertas.service';
+import {Router, ActivatedRoute} from '@angular/router';
+import {Oferta} from '../oferta';
+import 'rxjs/Rx';
+
 
 @Component({
-  selector: 'app-ofertas-detail',
-  templateUrl: './ofertas-detail.component.html',
-  styleUrls: ['./ofertas-detail.component.css']
+  selector: 'app-oferta-detail',
+  templateUrl: './oferta-detail.component.html',
+  styleUrls: ['./oferta-detail.component.css']
 })
-export class OfertasDetailComponent implements OnInit {
+export class OfertaDetailComponent implements OnInit {
+  errorMessage: string;
+  oferta: Oferta;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private ofertasService: OfertasService) {
+    this.oferta = <Oferta>{};
+  }
 
   ngOnInit() {
+    const ofertaId = this.route.snapshot.params['id'];
+    this.ofertasService.getOfertaById(ofertaId).subscribe(
+      oferta => this.oferta = oferta,
+      error => this.errorMessage = <any> error);
   }
+
+  gotoOfertaList() {
+    this.router.navigate(['/ofertas']);
+  }
+
+  editOferta() {
+    this.router.navigate(['/ofertas', this.oferta.id, 'edit']);
+  }
+
+  addOferta(oferta: Oferta) {
+    this.router.navigate(['/ofertas', oferta.id, 'oferta', 'add']);
+  }
+
 
 }
