2bc5b1fe9cff51930b5f30cefc957a3b26c60683
manulibra <manferfer96@gmail.com>
2019-09-27 12:43:02 +0200

    edit correcto

--- front/src/app/ofertas/ofertas-edit/ofertas-edit.component.ts
+++ front/src/app/ofertas/ofertas-edit/ofertas-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OfertasService} from '../ofertas.service';
+import {Oferta} from '../oferta';
+import {ActivatedRoute, Router} from '@angular/router';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-ofertas-edit',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ofertas-edit.component.css']
 })
 export class OfertasEditComponent implements OnInit {
+  oferta: Oferta;
+  errorMessage: string; // server error message
 
-  constructor() { }
+  constructor(private ofertaService: OfertasService, private route: ActivatedRoute, private router: Router) {
+    this.oferta = <Oferta>{};
+  }
 
   ngOnInit() {
+    const ofertaId = this.route.snapshot.params['id'];
+    this.ofertaService.getOfertaById(ofertaId).subscribe(
+      oferta => this.oferta = oferta,
+      error => this.errorMessage = <any> error);
   }
+
+  onSubmit(oferta: Oferta) {
+    var that = this;
+    this.ofertaService.updateOferta(oferta.id.toString(), oferta).subscribe(
+      res => this.gotoOfertaDetail(oferta),
+      error => this.errorMessage = <any> error
+    )
+  }
+
+  gotoOfertaDetail(oferta: Oferta) {
+    this.errorMessage = null;
+    this.router.navigate(['/ofertas', oferta.id]);
+  }
+
+
 
 }
