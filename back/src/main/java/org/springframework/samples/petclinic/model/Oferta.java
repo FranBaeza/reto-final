7317cc5a38792ce207a7fa8e61c6ff553adfc00b
cesbercar <formacion@DESKTOP-I3BSGFG.mshome.net>
2019-09-27 12:13:18 +0200

    JPA Oferta.java actualizado

--- back/src/main/java/org/springframework/samples/petclinic/model/Oferta.java
+++ back/src/main/java/org/springframework/samples/petclinic/model/Oferta.java
@@ -2,11 +2,43 @@ package org.springframework.samples.petclinic.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.rest.JacksonCustomVisitDeserializer;
+import org.springframework.samples.petclinic.rest.JacksonCustomVisitSerializer;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@Entity
+@Table(name = "visits")
+@JsonSerialize(using = JacksonCustomVisitSerializer.class)
+@JsonDeserialize(using = JacksonCustomVisitDeserializer.class)
 public class Oferta extends BaseEntity {
 	
+	@NotEmpty
+    @Column(name = "title")
 	private String title;
+	
+	@NotEmpty
+    @Column(name = "description")
 	private String description;
+	
+	@NotEmpty
+    @Column(name = "discount")
 	private double discount;
+	
+	@Column(name = "expireDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy/MM/dd")
 	private Date expireDate;
 	
 	public Oferta(String title, String description, double discount, Date expireDate) {
